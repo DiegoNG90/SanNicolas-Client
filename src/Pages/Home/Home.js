f41395cac167233a9c01c02da53747a7f3babd53
Diego <diego.nicolas.gandara@gmail.com>
2021-08-12 23:21:02 -0300

    Añado el Navbar en la /Page/Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
+import Navbar from '../../Components/Layout/Navbar'
+
 const Home = () => {
     return (
         <>
+        <Navbar />
         <h1>Home</h1>
         <p>Aca vienen todos los productos</p>
         </>
