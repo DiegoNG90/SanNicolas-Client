6ba458199cc56186baf992b244bc2362b90447f6
Diego <diego.nicolas.gandara@gmail.com>
2021-08-12 23:19:45 -0300

    Navbar con react-bootstrap y NavLink -react-router-dom-. Ademas, me creo un componente <Logo>

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,5 +1,28 @@
+import { Navbar as NavBootstrap, Container, Form, FormControl, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+import Logo from '../Logo';
+
 const Navbar = () => {
-    return (  );
-}
+  
+  return (
+    <NavBootstrap bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <NavLink to={'/'}> { /*activeClassName="selected" */}
+          <Logo />
+        </NavLink>
+        <Form className="d-flex justify-content-between">
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button className="ml-xxl-2" variant="outline-info">
+            Search
+          </Button>
+        </Form>
+        <NavLink to={'/login'}> { /*activeClassName="selected" */}
+          <Button variant="success">Admin</Button>
+        </NavLink>
+      </Container>
+    </NavBootstrap>
+  );
+};
 
 export default Navbar;
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import logo from './ugames_navbar.png';
+import './style.css'
+
+const Logo = () => {
+  return <img className="logo" src={logo} alt="Ugames logo"></img>;
+};
+
+export default Logo;
