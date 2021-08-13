8753a14d4187a21148296ca1c8bb01ece985ae5c
Diego <diego.nicolas.gandara@gmail.com>
2021-08-13 12:01:28 -0300

    Genero el contexto y el provider todo dentro de /context/Auth.js.

---
+++ src/context/Auth.js
@@ -0,0 +1,45 @@
+import { createContext, useReducer } from 'react';
+import { SET_AUTH, LOGOUT } from '../actions/auth';
+import { authReducer, initialState } from '../reducers/auth';
+import jwt_decode from 'jwt-decode';
+import API from '../api';
+
+// 1) Creo/defino el contexto
+export const AuthContext = createContext();
+// 2) Destructuro Provider de AuthContext
+const { Provider } = AuthContext;
+
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  const setAuth = ({ jwt }) => {
+    dispatch({ type: SET_AUTH, payload: { jwt } });
+  };
+
+  const getUserInformation = () => ({ name: jwt_decode(state.jwt) });
+
+  const logout = () => {
+    dispatch({ type: LOGOUT });
+    localStorage.removeItem('auth');
+    window.location.href = '/login';
+  };
+  const login = async ({ username, password }) => {
+    const { data: jwt } = await API.post('/auth', { username, password });
+    console.log('jwt: ', jwt);
+    if (jwt) {
+      setAuth({ jwt });
+      localStorage.setItem('auth', jwt);
+      return jwt;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <Provider value={{ state, setAuth, logout, login, getUserInformation }}>
+      {' '}
+      {children}{' '}
+    </Provider>
+  );
+};
