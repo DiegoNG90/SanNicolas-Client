00e80d73be8fe49c3fe777026e811611ebd24c72
Diego <diego.nicolas.gandara@gmail.com>
2021-08-13 11:58:57 -0300

    Armo el hook de useAxios para generar peticiones GET

---
+++ src/Hooks/useAxios.js
@@ -0,0 +1,28 @@
+import { useEffect, useCallback, useReducer } from 'react';
+import { API } from '../API';
+
+import { axiosReducer, initialState } from '../reducers/axios';
+import { ACTIONS } from '../actions/axios';
+
+export const useAxios = (endpoint = '') => {
+  const [state, dispatch] = useReducer(axiosReducer, initialState);
+
+  const getData = useCallback(async () => {
+    try {
+      const {data} = await API.get(`${endpoint}`);
+      console.log("Data from useAxios", data)
+      dispatch({ type: ACTIONS.SET_DATA, payload: {data} });
+    } catch (error) {
+      console.error(error);
+      dispatch({ type: ACTIONS.SET_ERROR });
+    }
+
+  }, [endpoint]);
+
+  useEffect(() => {
+    getData();
+  }, [endpoint, getData]);
+
+  return state;
+  // return [data, loading, error];
+};
