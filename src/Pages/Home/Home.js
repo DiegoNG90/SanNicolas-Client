48b7b39274fa935092371e6be23bdc3b966aaa47
Diego <diego.nicolas.gandara@gmail.com>
2021-08-13 12:03:04 -0300

    Empiezo a settear Home. Por ahora, voy probando, mockupeando y PRUEBO useAxios para que me traiga la array de todos los productos que tengo en mi servidor local. TODO OK por ahora :)

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
+import {useAxios} from "../../Hooks/useAxios"
+
 import Navbar from '../../Components/Layout/Navbar'
+import Products from '../../Components/Products'
+import Loader from '../../Components/Loader'
 
 const Home = () => {
+    const {data , loading, error} = useAxios("/products")
+    console.log("data", data, "loading", loading, "error", error)
     return (
         <>
         <Navbar />
-        <h1>Home</h1>
-        <p>Aca vienen todos los productos</p>
+        <Products />
+        <Loader />
         </>
      );
 }
