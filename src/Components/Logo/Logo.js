659bcd8c0e1409523797d91256dd27285ea1e799
Diego <diego.nicolas.gandara@gmail.com>
2021-08-14 20:51:14 -0300

    Añado un nuevo logo mas acorde al proyecto :D.

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,9 +1,9 @@
-import logo from './ugames_navbar.png';
+import sanNicolas_logo from './sannicolas_meli.png'
 import './style.css';
 import { memo } from 'react';
 
 const Logo = () => {
-  return <img className="logo" src={logo} alt="Ugames logo"></img>;
+  return <img className="logo" src={sanNicolas_logo} alt="Ugames logo"></img>;
 };
 
 export default memo(Logo);
