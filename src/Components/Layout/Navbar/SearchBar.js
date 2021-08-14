c3281bf97222ef2c7abfca8f6fe5de12f786d2d7
Diego <diego.nicolas.gandara@gmail.com>
2021-08-14 16:00:33 -0300

    Dentro del NavBar, va a haber un componente SearchBar que permitira hacer una busqueda en la db segun el endpoint y la palabra clave (i.e: /products?{endpoint}=) ol algo así...Empiezo a pensar la necesidad de crear un SerachContext (con su Hook useSearch) para que, tanto en las rutas Home(sin Auth) y AdminDashBoard (con Auth), pueda entrar a ambos y traer el endpoint y la palabra clave, para que basicamente el Search pueda ser usado en ambos lados.

---
+++ src/Components/Layout/Navbar/SearchBar.js
@@ -0,0 +1,37 @@
+import { FormControl, Button, Form } from 'react-bootstrap';
+import { useState, memo } from 'react';
+// import { useSearch } from '../../../Hooks/useSearch'
+// import { useAxios } from '../../../Hooks/useAxios'
+
+const SearchBar = () => {
+    const [ character,  setCharacter] = useState("")
+    console.log("character", character)
+    const getCharacters = (e) => {
+        e.preventDefault();
+        setCharacter(e.target.elements);
+    }
+
+    return (
+      <Form
+        onSubmit={getCharacters}
+        className="d-flex justify-content-between w-75"
+      >
+        <FormControl
+          onChange={(e) => setCharacter(e.target.value)}
+          type="text"
+          placeholder="Search"
+          className="mr-sm-2"
+        />
+        <Button
+          type="submit"
+          className="ml-xxl-2"
+          variant="outline-info"
+          placeholder="Place a product"
+        >
+          Search
+        </Button>
+      </Form>
+    );
+}
+
+export default memo(SearchBar);
