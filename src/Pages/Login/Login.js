9f4fbf0f1fa01a0f851524e6b36919b34a80b01f
Diego <diego.nicolas.gandara@gmail.com>
2021-08-15 18:55:10 -0300

    Arreglo que se renderice el mensaje de Admin/user incorrectos en caso que llegue null el jwt (commit en el servidor tmb)

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -8,11 +8,13 @@ import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   const [wrongPassword, setWrongPassword] = useState(false);
+  console.log("WrongPassword desde Login", wrongPassword)
   const { login } = useAuth();
   const history = useHistory();
 
   const handleLogin = async ({ username, password }) => {
     const jwt = await login({ username, password });
+    console.log('jwt desde handleLogin LOGIN', jwt);
     if (!jwt) {
     return setWrongPassword(true);
     } else {
@@ -30,53 +32,57 @@ const Login = () => {
 
   return (
     <>
-    <Button className="m-4" variant="info" onClick={() => history.push("/")}>
-      ← A Home
-    </Button>
-    <Container className="w-50 pt-5">
-      <h2>Admin Authentication</h2>
-      <Form onSubmit={formik.handleSubmit}>
-        <Form.Group
-          className="mb-3 justify-content-start"
-          controlId="formBasicEmail"
-        >
-          <Form.Label>Admin username</Form.Label>
-          <Form.Control
-            type="text"
-            name="username"
-            placeholder="Enter admin username"
-            onChange={formik.handleChange}
-            error={formik.errors.password}
-          />
-          {formik?.errors?.username && (
-            <Form.Text className="text-danger">
-              {formik.errors.username}
-            </Form.Text>
-          )}
-        </Form.Group>
+      <Button className="m-4" variant="info" onClick={() => history.push('/')}>
+        ← A Home
+      </Button>
+      <Container className="w-50 pt-5">
+        <h2>Admin Authentication</h2>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group
+            className="mb-3 justify-content-start"
+            controlId="formBasicEmail"
+          >
+            <Form.Label>Admin username</Form.Label>
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Enter admin username"
+              onChange={formik.handleChange}
+              error={formik.errors.password}
+            />
+            {formik?.errors?.username && (
+              <Form.Text className="text-danger">
+                {formik.errors.username}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={formik.handleChange}
-            error={formik.errors.password}
-          />
-          {formik?.errors?.password && (
-            <Form.Text className="text-danger">
-              {formik.errors.password}
-            </Form.Text>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={formik.handleChange}
+              error={formik.errors.password}
+            />
+            {formik?.errors?.password && (
+              <Form.Text className="text-danger">
+                {formik.errors.password}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
-      {wrongPassword && <span>Admin user o contraseña incorrectas</span>}
-    </Container>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+        {wrongPassword && (
+          <span className="text-danger">
+           <b> Admin user o contraseña incorrectas </b>
+          </span>
+        )}
+      </Container>
     </>
   );
 };
