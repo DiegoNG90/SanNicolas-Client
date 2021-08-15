ab4adee219b41b38112ccd8712fade0546e33bc7
Diego <diego.nicolas.gandara@gmail.com>
2021-08-15 17:39:38 -0300

    Dado que cree el contexto useData con el Provider Data, elimino useSearch y Serach Provider

--- src/Hooks/useSearch.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { SearchContext } from '../context/Search';
-
-export const useSearch = () => {
-  const context = useContext(SearchContext);
-  if (!context) throw new Error('useSearch must be wrapped with SearchProvider');
-  return context;
-};
