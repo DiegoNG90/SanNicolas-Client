05db9e03602cc808b29206e14289728464cecfa1
Diego <diego.nicolas.gandara@gmail.com>
2021-08-17 18:40:53 -0300

    Reemplazo los Select de bootstrap por simples Select de HTML5 puro

--- src/Pages/AdminDashboard/CreateTable/CreateTable.js
+++ src/Pages/AdminDashboard/CreateTable/CreateTable.js
@@ -1,7 +1,19 @@
-import { Form, Button, /* FloatingLabel */} from 'react-bootstrap';
+import { Form, Button /*, FloatingLabel */} from 'react-bootstrap';
+// import { FloatingLabel } from 'react-bootstrap/FloatingLabel';
 import { useState } from 'react';
 
 const CreateTable = () => {
+  const [precio, setPrecio] = useState(0);
+  const [decimales, setDecimales] = useState("")
+
+  const getPrecio = (e) => {
+    console.log(Number(e.target.value));
+    setPrecio(Number(e.target.value))
+    if(precio !== ""){
+      setDecimales(+precio.toFixed(2));
+    }
+  }
+
 
     const setDate = () => {
         let dd = new Date().getDate();
@@ -39,37 +51,56 @@ const CreateTable = () => {
 
         <Form.Group className="mb-3" controlId="precio">
           <Form.Label>Precio del producto</Form.Label>
-          <Form.Control type="text" placeholder="Precio" />
+          <Form.Control
+            type="text"
+            placeholder="Precio"
+            onChange={(e) => getPrecio(e)}
+          />
         </Form.Group>
 
         {/* onChange de precio y setea el value. readOnly*/}
 
         <Form.Group className="mb-3" controlId="decimales">
           <Form.Label>Decimales del producto</Form.Label>
-          <Form.Control type="text" value="" readOnly disabled />
+          <Form.Control type="text" value={decimales} readOnly disabled/>
         </Form.Group>
 
-        {/* <FloatingLabel controlId="floatingSelect" label="Works with selects">
-          <Form.Select aria-label="Floating label select example">
-            <option>Open this select menu</option>
-            <option value="ARS">Peso argentino</option>
-            <option value="USD">Dolar USD</option>
-          </Form.Select>
-        </FloatingLabel> */}
+        {/*<FloatingLabel controlId="floatingSelect" label="Works with selects">
+        <Form.Select aria-label="Floating label select example">
+          <option>Open this select menu</option>
+          <option value="ARS">Peso argentino</option>
+          <option value="USD">Dolar USD</option>
+        </Form.Select>
+        </FloatingLabel>*/}
         {/* No funciona, no lo toma y eso que le pase el FloatingLabel en el import de react-bootstrap! */}
-
-        <Form.Group className="mb-3" controlId="moneda">
+        {/* <Form.Group className="mb-3" controlId="moneda">
           <Form.Label>Moneda del producto</Form.Label>
           <Form.Control type="text" placeholder="ARS o USD" />
-        </Form.Group>
+        </Form.Group> */}
 
-        <Form.Group className="mb-3" controlId="estado">
+        <Form.Label>Moneda del producto</Form.Label>
+        <br />
+        <select className="mb-4" name="moneda" id="moneda">
+          <option value="ARS">Peso Argentino</option>
+          <option value="USD">Dolar USD</option>
+        </select>
+        <br />
+
+        {/* <Form.Group className="mb-3" controlId="estado">
           <Form.Label>Estado del producto</Form.Label>
           <Form.Control
             type="text"
             placeholder="Publicado, borrador o eliminado"
           />
-        </Form.Group>
+        </Form.Group> */}
+
+        <Form.Label>Estado del producto</Form.Label>
+        <br />
+        <select className="mb-4" name="estado" id="estado">
+          <option value="publicado">Publicado</option>
+          <option value="eliminado">Eliminado</option>
+          <option value="borrador">Borrador</option>
+        </select>
 
         <Form.Group className="mb-3" controlId="nuevo">
           <Form.Check type="checkbox" label="Es nuevo" />
@@ -77,10 +108,7 @@ const CreateTable = () => {
 
         <Form.Group className="mb-3" controlId="fecha_de_alta">
           <Form.Label>Fecha de alta del producto</Form.Label>
-          <Form.Control
-            type="text"
-            value={setDate()} readOnly disabled
-          />
+          <Form.Control type="text" value={setDate()} readOnly disabled />
         </Form.Group>
 
         <Button variant="success" type="submit">
