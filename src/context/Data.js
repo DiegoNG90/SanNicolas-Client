eae44fbc098a4ac0e6fb81678013cdcb0b9401f6
Diego <diego.nicolas.gandara@gmail.com>
2021-08-18 00:04:40 -0300

    Emprolijando Vol. II

--- src/context/Data.js
+++ src/context/Data.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect} from 'react';
+import { createContext } from 'react';
 import { useAxios } from '../Hooks/useAxios';
 
 
@@ -7,7 +7,7 @@ const { Provider } = DataContext;
 
 export const DataProvider = ({ children }) => {
   const { data, loading, error } = useAxios('/products');
-  
+
   return (
     <Provider value={{ data, loading, error }}>
       {children}
