245e0035e032a3babf5058e07d134f330f193a2e
Diego <diego.nicolas.gandara@gmail.com>
2022-04-04 02:08:50 -0300

    fix: remove unnecesary/unused code

--- src/Components/Layout/Navbar/SearchBar.js
+++ src/Components/Layout/Navbar/SearchBar.js
@@ -1,10 +1,7 @@
 import { FormControl, Button, Form } from 'react-bootstrap';
 import { memo } from 'react';
-import { useData } from '../../../Hooks/useData';
 
 const SearchBar = ({ setCharacter }) => {
-  const { data } = useData();
-
   const getCharacters = (e) => {
     e.preventDefault();
     setCharacter(e.target.elements[0].value);
@@ -16,7 +13,6 @@ const SearchBar = ({ setCharacter }) => {
       className="d-flex justify-content-center w-50"
     >
       <FormControl
-        // onChange={(e) => setCharacter(e.target.value)}
         type="text"
         placeholder="Busque productos por categoria ..."
         className="mr-sm-2"
